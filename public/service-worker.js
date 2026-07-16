4bf18e229d87236e92977aa6ca1c9178b9f4b053
mcalixto-lt <mcalixto.lt@gmail.com>
2026-07-16 14:39:49 -0300

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,15 @@
+// Limpador de compatibilidade: remove caches e service workers de entregas antigas.
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((key) => caches.delete(key)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    for (const client of clients) {
+      const url = new URL(client.url);
+      url.searchParams.set('ticket_atualizado', '140');
+      client.navigate(url.href);
+    }
+  })());
+});
